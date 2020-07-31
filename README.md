5c23eec5b359c6deeb8147ba2064cf12dcaf9bc1
Frederick Ernest <frederick.ernest@gmail.com>
2020-07-31 16:52:04 -0700

    add frederick_ernest.sql database file

--- README.md
+++ README.md
@@ -14,11 +14,11 @@ _Gather User input for many to one MySql database associateions_
 | :-------------      | :------------- | :------------- |
 | ** 1. Program will take user input to create a Stylist page, and correctly display a Stylists Page with details of the Stylist, and a list of clients belonging to the Stylist, when a Stylist is selected from the menu |"Frederick Ernest, Ethitics Specialist. | "Frederick Ernest, Ethitics Specialist. Clients: George, Larry, Nyx" |
 | ** 2. Program will take input allwoing the Salon Owner to add clients to a spesific stylist, and only if there are Stylists currently logged in the system| "Willow Jameson, 12/4/1975, @3:00pm, for frederick ernest" | "Willow Jameson has an Appt with Frederick Ernest at 3:00pm, 12/4/1975" |
-| ** 3. Program will not allow user to input Clients without first creating a Stylist pag | _user navigates to clients page_ | "There are no Stylists available for Booking, please try back later" |
+| ** 3. Program will not allow user to input Clients without first creating a Stylist pag | _user navigates to clients page_ | "There are no Stylists available for Booking, please try back later"e |
 
 ## Setup/Installation Requirements
 
-* _Clone or download this repository located at ***
+* _Clone or download this repository located at https://github.com/fetonecontrol/HairSalon.Solution_
 * _Run $ dotnet restore to download all dev dependencies_
 * _Run $ dotnet build to create project_
 
@@ -32,9 +32,14 @@ _{Please contact me at with any known bugs or support issues.}_
 
 ## Technologies Used
 
-* _GitHub_
 * _C#_
+* _Html_
+* _CSS_
+* _MySql
+* _Entity Framework_
+* _Razor_
+* _GitHub_
 
 ### License
 
-*Copyright (c) 2020 **_Hannah Beinstein MIT License_**
+*Copyright (c) 2020 **_Frederick-Ernest MIT License_**
---
+++ frederick_ernest.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE  IF NOT EXISTS `frederick_ernest` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `frederick_ernest`;
+-- MySQL dump 10.13  Distrib 8.0.15, for macos10.14 (x86_64)
+--
+-- Host: localhost    Database: frederick_ernest
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+INSERT INTO `clients` VALUES (3,'John',NULL,NULL,NULL,3),(5,'Onyx','Black',NULL,'Hair Cut',3),(6,'Finn','Wiliamms',NULL,'Motivational Speaking',3),(7,'Finn','Wiliamms',NULL,'Motivational Speaking',5);
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `stylists`
+--
+
+LOCK TABLES `stylists` WRITE;
+/*!40000 ALTER TABLE `stylists` DISABLE KEYS */;
+INSERT INTO `stylists` VALUES (3,'Frederick','Ernest',NULL,'Barbering'),(5,'John','Jacob',NULL,'Highmering/Smithing'),(7,'Jill','Jefferies',NULL,'Head, Shoulders Knees and Toes');
+/*!40000 ALTER TABLE `stylists` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-31 16:50:06
