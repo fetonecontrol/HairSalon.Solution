4b5d3b99f151a0a96416ea29a7bd1ffd32b34a6f
Frederick Ernest <frederick.ernest@gmail.com>
2020-07-31 11:15:27 -0700

    add some relaxing styling elements, render most links as buttons

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -7,9 +7,8 @@ namespace HairSalon.Models
   {
     public int Id { get; set; }
     public string FirstName { get; set; }
-
     public string LastName { get; set; }
-
+    public string Services { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
 
