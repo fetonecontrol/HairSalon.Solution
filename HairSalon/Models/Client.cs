9c8cc48f5cf55f614eea2a65a7d23a0b7a317130
Frederick Ernest <frederick.ernest@gmail.com>
2020-07-31 16:45:25 -0700

    end for the day, identify error that clients can be booked under more than one technician. Will be addressed later

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -8,6 +8,7 @@ namespace HairSalon.Models
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string ApptDate { get; set; }
     public string Services { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
